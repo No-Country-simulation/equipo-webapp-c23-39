ff79bae0ec55e6daa8e31fe9263b2f767fb949f4
Marjory Elena <109706655+EXlena7-7@users.noreply.github.com>
2025-01-16 22:43:09 -0400

    correciones del fronted

--- Front/app/page.tsx
+++ Front/app/page.tsx
@@ -1,13 +1,28 @@
+
+
+"use client";
 import InteractiveForm from "./components/InteractiveForm";
-import Particles from "../components/ui/particles";
 import { TextAnimate } from "../components/ui/text-animate";
 import { Bot } from "lucide-react";
 import Navbar from "./components/navbar/Navbar";
+import chatContentRef from "./components/InteractiveForm"
+import { useRef } from "react";
 
 export default function Home() {
+  
+  const chatContentRef = useRef<HTMLDivElement>(null);
+  const scrollToBottom = () => {
+    if (chatContentRef.current) {
+      chatContentRef.current.scrollTo({
+        top: chatContentRef.current.scrollHeight,
+        behavior: "smooth", // Suave desplazamiento
+      });
+    }
+  };
+  
   return (
     <div
-      className="flex flex-col justify-between items-center w-full h-screen"
+      className="flex flex-col w-full h-screen"
       style={{
         backgroundImage: "url('/fondo-chat.jpeg')",
         backgroundSize: "cover",
@@ -15,29 +30,31 @@ export default function Home() {
       }}
     >
       <Navbar />
-
-      <div className="relative z-10 flex flex-col w-full h-full max-w-4xl px-4 py-6 overflow-y-auto">
-        <div className="flex flex-col items-center mb-6">
+      <div className="relative flex flex-col w-full h-full max-w-4xl mx-auto">
+      
+        <div className="flex flex-col items-center mb-4 mt-6">
           <Bot size={50} color="black" />
           <TextAnimate
             animation="blurInUp"
             by="character"
-            className="text-white text-4xl font-extrabold text-center mt-4 leading-tight sm:text-2xl md:text-3xl lg:text-4xl xl:text-6xl"
+            className="text-white text-4xl font-extrabold text-center mt-4 leading-tight sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl"
           >
-            ¿En qué puedo ayudarte?
+            ¿Que Lugar te Gustaria Visitar?
           </TextAnimate>
         </div>
+
+     
         <div
           id="chat-content"
-          className="flex-grow overflow-y-auto p-4 space-y-4 bg-opacity-60 bg-black rounded-lg"
-          style={{
-            maxHeight: "calc(100vh - 150px)", 
-          }}
+          ref={chatContentRef}
+          className="flex-grow overflow-y-auto p-3 space-y-4 bg-opacity-60 bg-black rounded-lg"
         >
-        <InteractiveForm />
+        
+          <InteractiveForm />
         </div>
-
       </div>
+
     </div>
   );
 }
+
