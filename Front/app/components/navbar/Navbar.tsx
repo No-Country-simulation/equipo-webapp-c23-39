5ebc8db6b3f9e1b777dc2382af281e70b0b2ef20
Marjory Elena <109706655+EXlena7-7@users.noreply.github.com>
2025-01-23 11:55:29 -0400

    mejoras de la IU del Chatbot

--- Front/app/components/navbar/Navbar.tsx
+++ Front/app/components/navbar/Navbar.tsx
@@ -1,19 +1,24 @@
 'use client';
 import Link from "next/link";
 import { Bot, Send} from 'lucide-react'
+import Image from "next/image";
 
 
 export default function Navbar() {
 
   return (
-    <nav className="w-full flex bg-white bg-opacity-30 p-2">
+    <nav className="w-full flex text-white  bg-opacity-30 p-2">
         <Link href={'/'} className="flex items-center">  
-        
-        <Bot size={30} color="black" />
+        <div className="absolute top-0 left-0 p-2">
+          <Image
+                  src="/Logo.png" 
+                  alt="Descripción de la imagen"
+                  width={85} 
+                  height={85} 
+                  
+                  />
+        </div>
         </Link>
-        <div className="flex p-1"></div>
-        <p className="font-extrabold">MigoTrip</p>
-       
     </nav>
   )
 }
--- Front/app/middleware.ts
+++ Front/app/middleware.ts
@@ -1,17 +1,25 @@
 
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
 
+export function middleware(request: NextRequest){
+  return NextResponse.redirect(new URL("/login", request.url ))
+}
 
-const protectedRoutes = ["/register", "/login"];
+export const config ={
+  matcher:['/auth/login', '/auth/register']
+}
 
-export function middleware(request: NextRequest) {
-  const token = request.cookies.get("auth-token");
+// const protectedRoutes = ["/register", "/login"];
+
+// export function middleware(request: NextRequest) {
+//   const token = request.cookies.get("auth-token");
 
   
-  if (!token && protectedRoutes.includes(request.nextUrl.pathname)) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+//   if (!token && protectedRoutes.includes(request.nextUrl.pathname)) {
+//     return NextResponse.redirect(new URL("/login", request.url));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
